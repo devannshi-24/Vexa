4e989493c0ca59a8a8b4341db8578ecc08abe874
devannshi-24 <devannshii24@gmail.com>
2026-05-11 22:13:39 +0530

    setup backend

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { login, logout, signUp } from '../controllers/auth.controller.js'
+const authRouter = express.Router()
+
+authRouter.post('/login',login)
+authRouter.post('/signup',signUp)
+authRouter.get('/logout',logout)
+
+export default authRouter
