f48d29c4e3a724177595a72c5e7b18e8abcfa77a
devannshi-24 <devannshii24@gmail.com>
2026-07-31 02:43:56 +0530

    fixed CORS error

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js";
 
 const app = express();
 app.use(cors({
-    origin:["http://localhost:5173","vexa-lemon.vercel.app"],
+    origin:["http://localhost:5173","https://vexa-lemon.vercel.app"],
     credentials:true
 }))
 
